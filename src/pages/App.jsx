91ce4e634694d9c5c428e2a2363607bb0dd39024
Ítalo Ryan <italoryansouza2019@gmail.com>
2024-12-05 15:36:36 -0300

    Initial commit

---
+++ src/pages/App.jsx
@@ -0,0 +1,14 @@
+import './../output.css'
+
+function App() {
+
+  return (
+    <>
+      <p className="text-red-950 font-mono">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
